6aec2607a60c6aed963d76d66825d3519b02c17b
Angel G300 Saucedo <saucedog926@gmail.com>
2024-05-08 22:14:24 -0300

    refactorizando un poco nomas.

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import class_video as c
 ## al ejecutar el codigo, la consola se queda esperando un link de una playlist publica o no listada, de youtube para poder descargar sus
 ## videos mp3 de la lista.
 
+
+### trabajar excepsiones
 while True:
+    link  = input("Ingresa el link de la playlist.\nTiene que estar Publica o No listada; ")
     try:
-        link  = input("Ingresa el link de la playlist.\nTiene que estar Publica o No listada; ")
         miPlaylist = c.Cargar_playlist(link); break
     except: print("ocurrio un error - intenta poner un link valido"); continue
 
-miPlaylist.preparar_y_descargar_todo()
+miPlaylist.setup()
 
-print("se convertira todo a mp3\n")
-conversor = c.Convertir(miPlaylist.videos_obj)
-conversor.convertir_todo_a_mp3()
-c.Compresor().zip(conversor.get_path())
+convertir = c.Convertir(miPlaylist.videos_obj)
+convertir.todo_mp3() ## deberia trabajar con hilos.
+c.Compresor().zip(convertir.path)
 
 print("FIN DEL PROGRAMA")
 print("FIN DEL PROGRAMA")
